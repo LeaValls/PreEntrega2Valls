6f23050fc31f3822c3d209d9e9135e44eec0548a
LeaValls <leandro_valls@hotmail.com>
2023-03-16 22:23:58 -0300

    subiendo el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./componentes/NavBar";
+
+function app () {
+  return (
+    <div>
+      <Navbar />     
+      
+    </div>
+  )
+}
+
+export default app;
---
+++ src/componentes/NavBar/index.jsx
@@ -0,0 +1,21 @@
+import "./navbar.css";
+
+
+function NavBar () {
+    return (
+        <div className="menu">
+            <ul className="lista">
+                <li>
+                    <button>Productos</button>
+                </li>
+                <li>
+                    <button>Contacto</button>
+                </li>
+            </ul>
+            
+        </div>
+        
+    )
+}
+
+export default NavBar;
